28c445ec7911be327ee9cf52eca3962b0afcfc55
Matheus Coutinho <lammerlopes@hotmail.com>
2024-10-01 10:31:07 -0300

    Desgin Patterns TypeScript

---
+++ src/creational/factory-method/factories/vehicle-factory.ts
@@ -0,0 +1,12 @@
+// Interface é mais leve que uma classe Abstrata
+import { Vehicle } from '../vehicle/vehicle';
+
+export abstract class VehicleFactory {
+  abstract getVehicle(vehicleName: string): Vehicle;
+
+  pickUp(customerName: string, vehicleName: string): Vehicle {
+    const car = this.getVehicle(vehicleName);
+    car.pickUp(customerName);
+    return car;
+  }
+}
