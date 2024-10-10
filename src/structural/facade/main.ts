cec72d0646b47b327036d6f45dd14e2aefeae8a2
Matheus Coutinho <lammerlopes@hotmail.com>
2024-10-10 11:03:19 -0300

    Design Patterns TypeScript

---
+++ src/structural/facade/main.ts
@@ -0,0 +1,8 @@
+import { BuilderFacade } from './builder-facade';
+
+const builderFacade = new BuilderFacade();
+
+builderFacade.makeMeal1();
+builderFacade.makeMeal2();
+builderFacade.makeMeal3();
+// console.log(builderFacade.makeMeal2());
